6fc69c0412b3bd9bcad9976d643fa984dd8d5ab4
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2024-04-06 14:46:51 -0300

    Create altura.java

---
+++ Etec-2023/EstudoCaso/altura.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class altura {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		int pedro=1.45, joao=1.34, ano=1 ;
+		
+		pedro= pedro*100;
+		System.out.println(pedro);
+	}
+}
