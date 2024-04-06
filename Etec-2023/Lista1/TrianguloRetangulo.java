f55c3c53ae1f18d75c4413c7888d9f69cec2920c
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2024-04-06 14:55:23 -0300

    Add files via upload

---
+++ Etec-2023/Lista1/TrianguloRetangulo.java
@@ -0,0 +1,10 @@
+
+public class TrianguloRetangulo {
+	public static void main(String args[]) {
+		double base, h, area;
+		base = 10;
+		h = 15;
+		area = (base*h)/2;
+		System.out.println("A área calculada é "+area+" metros");
+	}
+}
---
+++ Etec-2023/Lista1/TrocaDados.java
@@ -0,0 +1,13 @@
+
+public class TrocaDados {
+	public static void main(String args[]) {
+		double a, b, valorAux;
+		a = 5;
+		b = 10;
+		valorAux = 5; 
+		a = 10;
+		b = 5;
+		System.out.println("Dados trocados das variaveis "+a+" e "+b);
+	}
+}
+
