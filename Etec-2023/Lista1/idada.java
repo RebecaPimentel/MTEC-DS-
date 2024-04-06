a34cdc665e36fa74b6a082596d340056981310d4
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2024-04-06 14:55:08 -0300

    Create idada.java

---
+++ Etec-2023/Lista1/idada.java
@@ -0,0 +1,10 @@
+
+public class Idade {
+	public static void main(String args[]) {
+		int anoAtual, anoNascimento, idade;
+		anoAtual = 2023;
+		anoNascimento = 2007;
+		idade = anoAtual-anoNascimento;
+		System.out.println("A idade calculada é "+idade+" anos");
+	}
+}
