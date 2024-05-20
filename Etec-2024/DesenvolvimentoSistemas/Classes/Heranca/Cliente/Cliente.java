84c74cad4dfa177ac7317c2bf88b231eb14fa755
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2024-05-20 17:58:38 -0300

    Add files via upload

---
+++ Etec-2024/DesenvolvimentoSistemas/Classes/Heranca/Cliente/Cliente.java
@@ -0,0 +1,40 @@
+public class Cliente {
+	
+    private String nome;
+    private int idade;
+    private String endereco;
+    private double valor_pedido;
+    private double valor_promocional;
+
+    public Cliente(String nome, int idade, String endereco, double valor_pedido, double valor_promocional) {
+    	
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.valor_pedido = valor_pedido;
+        this.valor_promocional = valor_promocional;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor_pedido() {
+        return valor_pedido;
+    }
+
+    public double getValor_promocional() {
+        return valor_promocional;
+    }
+
+    public void calculaDesconto() {
+        double desconto = valor_pedido * 0.1;
+        double valorComDesconto = valor_pedido - desconto;
+        System.out.println(nome + " tem direito a um desconto de 10%: R$" + desconto);
+        System.out.println("Valor do pedido com desconto: R$" + valorComDesconto);
+    }
+}
+
+
+
---
+++ Etec-2024/DesenvolvimentoSistemas/Classes/Heranca/Cliente/ClientePremium.java
@@ -0,0 +1,20 @@
+
+public class ClientePremium extends Cliente {
+	
+	public ClientePremium(String nome, int idade, String endereco, double valor_pedido, double valor_promocional) {
+        super(nome, idade, endereco, valor_pedido, valor_promocional);
+    }
+
+    @Override
+    public void calculaDesconto() {
+        double desconto = getValor_pedido() * 0.15;
+        double valorDesconto = getValor_pedido() - desconto;
+        System.out.println(getNome() + " tem direito a um desconto de 15%: R$" + desconto);
+        System.out.println("Valor do pedido com desconto: R$" + valorDesconto);
+    }
+    
+}
+
+
+
+
