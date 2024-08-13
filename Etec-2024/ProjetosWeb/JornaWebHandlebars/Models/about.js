006ad8c3978c32bcc9cf9b4def26bbd1f9f53709
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2024-08-12 23:41:17 -0300

    Create about.js

---
+++ Etec-2024/ProjetosWeb/JornaWebHandlebars/Models/about.js
@@ -0,0 +1,34 @@
+export const about = [
+    {
+        "titulo": " e Valores",
+        "span": "Missão" ,
+        "desc": "A missão do Centro Paula Souza é promover a educação profissional e tecnológica para o desenvolvimento humano e socioeconômico, contribuindo para a formação de cidadãos conscientes e preparados para enfrentar os desafios do mercado de trabalho. Os valores que norteiam a instituição incluem a excelência acadêmica, a inovação, a inclusão social, a sustentabilidade e a responsabilidade social.",
+        "imagem": "http://localhost:3000/imgs/imgFofocas/fofokinha.png",
+    },
+    {
+        "titulo": "  O  tem tudo que você precisa saber.",
+        "span": "Fofokinha" ,
+        "desc": "A pagina fofokinha tem como objetivo informar sobre os acontecimentos no mundo das celebridades.",
+        "imagem": "http://localhost:3000/imgs/imgFofocas/fofokinha.png",
+    },
+    {
+        "titulo": "  A  é uma das datas mais esperadas do ano.",
+        "span": "Festa Junina" ,
+        "desc": "A festa junina é uma das celebrações mais tradicionais e populares do Brasil, realizada anualmente durante o mês de junho.",
+        "imagem": "http://localhost:3000/imgs/imgDtsC/logoJunina.png",
+    },
+    {
+        "titulo": "   em São Paulo.",
+        "span": "Eventos Culturais" ,
+        "desc": "Os eventos culturais que acontecem no estado de São Paulo.",
+        "imagem": "http://localhost:3000/imgs/imgEventosC/eventosLogo.png",
+    },
+    {
+        "titulo": " O  tem muito  a oferecer.",
+        "span": "Centro Paula Souza" ,
+        "desc": "Os cursos do Centro Paula Souza nas Etecs.",
+        "imagem": "http://localhost:3000/imgs/imgCps/logoCps.png",
+    }
+]
+
+
