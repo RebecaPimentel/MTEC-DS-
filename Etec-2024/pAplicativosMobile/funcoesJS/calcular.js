c41aafef4455049439052a699ab7efac0fd7493c
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2024-09-27 07:44:57 -0300

    Add files via upload

---
+++ Etec-2024/pAplicativosMobile/funcoesJS/calcular.js
@@ -0,0 +1,32 @@
+function adicao() {
+    var num1 = 1;
+    var num2 = 2;
+    var resultado =  num1 + num2;
+    console.log("Resultado da soma: "+ num1 + " + " + num2 + " = " + resultado);
+}
+function sub() {
+    var num1 = 11;
+    var num2 = 2;
+    var resultado =  num1 - num2;
+    console.log("Resultado da subtração : "+ num1 + " - " + num2 + " = " + resultado);
+}
+function mult() {
+    var num1 = 9;
+    var num2 = 2;
+    var resultado =  num1 * num2;
+    console.log("Resultado da multplicação : "+ num1 + " * " + num2 + " = " + resultado);
+}
+function div() {
+    var num1 = 8;
+    var num2 = 2;
+    var resultado =  num1 / num2;
+    console.log("Resultado da divisão : "+ num1 + " / " + num2 + " = " + resultado);
+}
+
+console.log(" ");
+
+adicao();
+sub();
+mult();
+div();
+
