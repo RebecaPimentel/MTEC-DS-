b2473f8a5fa4a351662a2aed79d83e2943765240
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2025-02-17 09:25:30 -0300

    Add files via upload

---
+++ Etec-2025/PW3/Calculadora/script.js
@@ -0,0 +1,26 @@
+const visor = document.getElementById("input");
+
+const del = document.querySelector("#delete");
+const apagarTd = document.querySelector("#apagarTd");
+const porc = document.querySelector("#porc");
+const div = document.querySelector("#div");
+const sete = document.querySelector("#sete");
+const oito = document.querySelector("#oito");
+const nove = document.querySelector("#nove");
+const mult = document.querySelector("#mult");
+const quatro = document.querySelector("#quatro");
+const cinco = document.querySelector("#cinco");
+const seis = document.querySelector("#seis");
+const sub = document.querySelector("#sub");
+const um = document.querySelector("#um");
+const dois = document.querySelector("#dois");
+const tres = document.querySelector("#tres");
+const ad = document.querySelector("#ad");
+const zero = document.querySelector("#zero");
+const ponto = document.querySelector("#ponto");
+const resuzerol = document.querySelector("#resul");
+
+
+function one() {
+    visor.value = "1";
+}
