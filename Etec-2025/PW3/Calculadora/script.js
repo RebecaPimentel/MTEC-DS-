36f977c40f63f8e9359286a1864c7fdda8f82652
Rebeca Pimentel <145283254+RebecaPimentel@users.noreply.github.com>
2025-02-17 21:28:41 -0300

    Update script.js

--- Etec-2025/PW3/Calculadora/script.js
+++ Etec-2025/PW3/Calculadora/script.js
@@ -1,26 +1,27 @@
-const visor = document.getElementById("input");
+const visor = document.getElementById("visor");
 
-const del = document.querySelector("#delete");
-const apagarTd = document.querySelector("#apagarTd");
-const porc = document.querySelector("#porc");
-const div = document.querySelector("#div");
-const sete = document.querySelector("#sete");
-const oito = document.querySelector("#oito");
-const nove = document.querySelector("#nove");
-const mult = document.querySelector("#mult");
-const quatro = document.querySelector("#quatro");
-const cinco = document.querySelector("#cinco");
-const seis = document.querySelector("#seis");
-const sub = document.querySelector("#sub");
-const um = document.querySelector("#um");
-const dois = document.querySelector("#dois");
-const tres = document.querySelector("#tres");
-const ad = document.querySelector("#ad");
-const zero = document.querySelector("#zero");
-const ponto = document.querySelector("#ponto");
-const resuzerol = document.querySelector("#resul");
+function ad(digitos) {
+    visor.value += digitos.innerText;
+} 
 
+function zero() {
+    visor.value += "0";
+}
 
-function one() {
-    visor.value = "1";
+function apagarTudo() {
+    visor.value = "";
+}
+
+function del() {
+    if (visor.value.length > 0) {
+        visor.value = visor.value.slice(0, -1);
+    }
+}
+
+function resul() {
+    try{
+        visor.value = eval(visor.value);
+    } catch {
+        alert("Error com a conta: " + visor.value);
+    }
 }
